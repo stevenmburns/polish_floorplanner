4f774dc58e142fbf31d7879957b5178de668ae3f
Steven Burns <smburns47@gmail.com>
2019-09-17 00:42:48 -0700

    Fixed up the stuttering cases

--- polish/piecewise.py
+++ polish/piecewise.py
@@ -1,5 +1,7 @@
 
 import bisect
+import logging
+from collections import defaultdict
 
 #
 # We'll assume there is a vertical line at p(0), and a horizontal line at p(-1)
@@ -19,6 +21,7 @@ import bisect
 
 class Piecewise:
     def __init__(self, points):
+
         self.xs = [ p[0] for p in points]
         self.ys = [ p[1] for p in points]
 
@@ -29,47 +32,75 @@ class Piecewise:
                 assert not (r == s == t)
 
         if len(self.xs) > 1:
-            xpairs = zip( self.xs[:-1], self.xs[1:])
-            ypairs = zip( self.ys[:-1], self.ys[1:])
+            xpairs = list(zip( self.xs[:-1], self.xs[1:]))
+            ypairs = list(zip( self.ys[:-1], self.ys[1:]))
 
 # Check non-decreasing x and non-increasing y
             assert all( r<=s for (r,s) in xpairs)
             assert all( r>=s for (r,s) in ypairs)
 
 #  We don't have a stutter in both dimensions in the same transition
-            assert all( not (r == s and u == v) for ((r,s),(u,v)) in zip(xpairs,ypairs))
 
+            assert all( not (r == s and u == v) for ((r,s),(u,v)) in zip(xpairs,ypairs))
 
     def p( self, i):
         return self.xs[i], self.ys[i]
 
     @staticmethod
-    def compute( p0, p1, x):
+    def compute( p0, p1, x, op=min):
         x0, y0 = p0
         x1, y1 = p1
 
         if x0 == x1:
             assert x == x0, (p0,p1,x)
-            return y1
+            return op(y0,y1)
         else:
             return y0 + (y1-y0)/(x1-x0)*(x-x0)
 
-    def __call__(self, x):
+    def __call__(self, x, func=min):
         q = bisect.bisect( self.xs, x)
 
         if q == 0:
             raise Exception(f"Value out of range: {x} < {self.xs[0]}")
         if q == len(self.xs):
-            return self.ys[-1]
+            if x > self.xs[-1]:
+                return self.ys[-1]
+            else:
+                assert x == self.xs[-1]
+                if q == 1:
+                    return self.ys[0]
+                q -= 1
 
-        return Piecewise.compute( self.p(q-1), self.p(q), x)
+        # special stuttering case
+        if q > 1 and  x == self.xs[q-1] and x == self.xs[q-2]:
+            q -= 1
+
+        assert q > 0
+        return Piecewise.compute( self.p(q-1), self.p(q), x, func)
 
     def __add__(self, other):
 
 # Assert domain is identical: relax later        
 
-        assert self.xs == other.xs
+        domain0 = defaultdict(int)
+        for x in self.xs:
+            domain0[x] += 1
+        domain1 = defaultdict(int)
+        for x in other.xs:
+            domain1[x] += 1
 
-        points = [ (u,r+s) for (u,r,s) in zip( self.xs, self.ys, other.ys)]
-            
+        logging.info( f"Before: {domain0} {domain1}")
+
+        for (k,v) in domain1.items():
+            domain0[k] = max( domain0[k], v)
+
+        logging.info( f"After: {domain0} {domain1}")
+
+        points = []
+        for (k,v) in sorted(domain0.items()):
+            assert v <= 2
+            if v == 2:
+                points.append( (k, self(k,max) + other(k,max)))
+            points.append( (k, self(k) + other(k)))
+
         return Piecewise( points)
--- tests/test_piecewise.py
+++ tests/test_piecewise.py
@@ -5,14 +5,14 @@ from polish import Piecewise
 
 def test_piecewise_empty():
     with pytest.raises(Exception):
-        pw = Piecewise()
+        Piecewise()
 
 def test_piecewise_hyperbola():
 
     pw = Piecewise( [ (0.5,2), (1,1), (2,0.5)])
 
     with pytest.raises(Exception): 
-        assert pw(  0)   == 3
+        pw(  0)
 
     assert pw( 0.5)  == 2
     assert pw(  1) == 1
@@ -52,8 +52,51 @@ def test_piecewise_sum_irregular():
     pwA = Piecewise( [ (0.5,2), (.9,1), (2,0.5)])
     pwB = Piecewise( [ (0.5,2), (1.1,1.5), (2,0.5)])
 
-    with pytest.raises(AssertionError):
-        pw = pwA + pwB
+    pw = pwA + pwB
+
+    assert pw( 0.5) == 4
+    assert abs( pw( 0.9) - 2.6666667) < 0.0001
+    assert abs( pw( 1.1) - 2.4090909) < 0.0001
+
+def test_piecewise_sum_irregular2():
+
+    pwA = Piecewise( [ (1,2), (2,2), (2,1)])
+    pwB = Piecewise( [ (1,2), (2,2), (2,1)])
+
+    pw = pwA + pwB
+
+    assert pw( 1) == 4
+    assert pw( 1.99) == 4
+    assert pw( 2) == 2
+    assert pw( 2.01) == 2
+    assert pw( 3) == 2
+
+def test_piecewise_single_point():
+    pw = Piecewise( [ (1,1)])
+    assert pw( 1) == 1
+    assert pw( 1.01) == 1
+    with pytest.raises(Exception):
+        pw( 0.09)
+
+def test_piecewise_sum_irregular3():
+
+    pwA = Piecewise( [ (1,2), (2,2), (2,1)])
+    pwB = Piecewise( [ (1,2), (1.5,2), (1.5,1), (2,1)])
+
+    assert pwB( 1.5, min) == 1
+    assert pwB( 1.51, min) == 1
 
 
+    assert pwA( 1.5, max) == 2
+    assert pwB( 1.5, max) == 2
+    assert pwB( 1.51, max) == 1
 
+
+    pw = pwA + pwB
+
+    assert pw( 1) == 4
+    assert pw( 1.49) == 4
+    assert pw( 1.50) == 3
+    assert pw( 1.51) == 3
+
+    assert pw( 3) == 2
